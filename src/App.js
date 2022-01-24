884351713593ecc1320314f8ad5891d4228a6ba2
Frederik Reich <frederikreich@googlemail.com>
2022-01-24 11:45:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        HALLO CRAZYCODERS!
+        HALLO CRAZYCODERS! :)
       </header>
     </div>
   );
